18431e8ce9c0a4823e561d27a53c11fe2e6a249d
ffozildzhanov <ffozildzhanov@beeline.uz>
2021-07-06 14:35:57 +0500

    добавил логин рекуайрд к урлам

--- database_separator/urls.py
+++ database_separator/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth.decorators import login_required
 
 from .views import DataBaseDetailView, SeparationView, CheckView
 
@@ -6,7 +7,7 @@ from .views import DataBaseDetailView, SeparationView, CheckView
 app_name = 'database_separator'
 
 urlpatterns = [
-    path('database/<int:pk>', DataBaseDetailView.as_view(), name='db'),
-    path('separate', SeparationView.as_view(), name='separate'),
-    path('check/', CheckView.as_view(), name='check')
+    path('database/<int:pk>', login_required(DataBaseDetailView.as_view()), name='db'),
+    path('separate', login_required(SeparationView.as_view()), name='separate'),
+    path('check/', login_required(CheckView.as_view()), name='check')
 ]
