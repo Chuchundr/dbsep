4ac70e3dbcba885ef5a9445841cd98f9690e2c35
ffozildzhanov <ffozildzhanov@beeline.uz>
2021-12-26 13:17:08 +0500

    добавил возможность поиска в админке

--- database_separator/admin.py
+++ database_separator/admin.py
@@ -10,24 +10,29 @@ class TablesTabularInline(admin.TabularInline):
 @admin.register(SequenceRange)
 class SequenceRangeAdmin(admin.ModelAdmin):
     list_display = ['database', 'start', 'max' ]
+    search_fields = ['database', ]
 
 
 @admin.register(DataBase)
 class DataBaseAdmin(admin.ModelAdmin):
     list_display = ['name', ]
+    search_fields = ['name', ]
 
 
 @admin.register(Publication)
 class PublicationAdmin(admin.ModelAdmin):
     inlines = [TablesTabularInline, ]
     list_display = ['name', ]
+    search_fields = ['name', ]
 
 
 @admin.register(Subscription)
 class SubscriptionAdmin(admin.ModelAdmin):
     list_display = ['name', ]
+    search_fields = ['name', ]
 
 
 @admin.register(ReplicationSlot)
 class ReplicationSlotAdmin(admin.ModelAdmin):
     list_display = ['name', ]
+    search_fields = ['name', ]
